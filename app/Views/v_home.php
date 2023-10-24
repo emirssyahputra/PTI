97a8730f8111f79c2a3345759cbf2d2d791cf2d4
EMIRSSYAH PUTRA <emirssyah13@gmail.com>
2023-10-25 01:31:49 +0700

    update

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -427,27 +427,26 @@
 
   <!-- Javascript Files
   ================================================== -->
-
   <!-- initialize jQuery Library -->
-  <script src="plugins/jQuery/jquery.min.js"></script>
+  <script src="<?php echo base_url().'plugins/jQuery/jquery.min.js'?>"></script>
   <!-- Bootstrap jQuery -->
-  <script src="plugins/bootstrap/bootstrap.min.js" defer></script>
+  <script src="<?php echo base_url().'plugins/bootstrap/bootstrap.min.js'?>"></script>
   <!-- Slick Carousel -->
-  <script src="plugins/slick/slick.min.js"></script>
-  <script src="plugins/slick/slick-animation.min.js"></script>
+  <script src="<?php echo base_url().'plugins/slick/slick.min.js'?>"></script>
+  <script src="<?php echo base_url().'plugins/slick/slick-animation.min.js'?>"></script>
   <!-- Color box -->
-  <script src="plugins/colorbox/jquery.colorbox.js"></script>
+  <script src="<?php echo base_url().'plugins/colorbox/jquery.colorbox.js'?>"></script>
   <!-- shuffle -->
-  <script src="plugins/shuffle/shuffle.min.js" defer></script>
+  <script src="<?php echo base_url().'plugins/shuffle/shuffle.min.js'?>"></script>
 
 
   <!-- Google Map API Key-->
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
   <!-- Google Map Plugin-->
-  <script src="plugins/google-map/map.js" defer></script>
+  <script src="<?= base_url('plugins/google-map/map.js'); ?>" defer></script>
 
   <!-- Template custom -->
-  <script src="js/script.js"></script>
+  <script src="<?php echo base_url().'js/script.js'?>"></script>
 
   </div><!-- Body inner end -->
   </body>
