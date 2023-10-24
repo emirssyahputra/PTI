aa3a16cadca34c8fa70da903be49d0c1a315f567
EMIRSSYAH PUTRA <emirssyah13@gmail.com>
2023-10-25 01:06:20 +0700

    Initial commit

---
+++ app/Views/v_login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/style.css'); ?>">
+    <link rel="icon" type="image/png" href="<?= base_url('images/favicon.png'); ?>">
+    <script src="https://kit.fontawesome.com/c0e27fec68.js" crossorigin="anonymous"></script>
+    <title>Login</title>
+</head>
+
+<body>
+    <div class='login-outer-container'>
+        <div class='login-container'>
+            <div class='login-area'>
+                <img src="<?= base_url('images/logo.png'); ?>" alt="RedX Logo" class="centered-logo">
+                <h3>Login</h3>
+                <form class='login-items'>
+                    <label htmlFor="username">Email</label>
+                    <input type="email" class='login' name="email" placeholder='email@gmail.com' required />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" class='login' name="password" placeholder="Password" required />
+                    <input type="submit" class='login-btn' value="Login" />
+                </form>
+                <p class='p'>Belum punya akun? <a class='a' href="<?php echo site_url('Register');?>">Register</a></p>
+                <p class='p'>Lupa password? <a class='a' href="<?php echo site_url('Lupa');?>">Reset Password</a></p>
+                <p class='p'><a class='a' href="<?php echo site_url('/');?>">Kembali ke Home</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
