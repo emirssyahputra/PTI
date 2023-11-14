4d98d2b0413dbf83ca33e31022897d615e67d3b3
EMIRSSYAH PUTRA <emirssyah13@gmail.com>
2023-11-14 16:46:59 +0700

    fix fitur career di home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,9 +7,9 @@ class Home extends BaseController
 
     public function index()
     {
-        $m_career = new \App\Models\M_Career(); // Membuat objek model M_Career
+        $lowongan = new \App\Models\M_Career(); // Membuat objek model M_Career
         $m_outlet = new \App\Models\M_outlet();
-        $data['loker'] = $m_career->get_all_lowongan(); // Mengambil semua data lowongan
+        $data['loker'] = $lowongan->get_lowongan_berjalan();        
         $data['tot_outlet'] = $m_outlet -> countAllResults(); // Menambahkan data jumlah outlet ke $data
         return view('v_home', $data);
     }
